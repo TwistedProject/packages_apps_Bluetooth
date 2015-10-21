05a20613186d7b0221338562236f1745c7716fc1
Satish Kodishala <skodisha@codeaurora.org>
2015-10-21 18:41:26 +0530

    Let audio manager know when BT is turned off
    Usecase:
    1. Pair and connect hf client from remote AG
    2. Make a MO call
    3. Airplane mode on-off
    4. connect to hf client from remote device
    
    Failure:
    After airplane mode is off, BT will turn on and SCO connection
    will be established since there is active call. SCO connection
    is failing during this time.
    
    Rootcause:
    SCO path setup failing in hf client since earlier sco path before BT was
    turned off not cleaned up.
    
    Fix:
    When SCO is established with device as hf client and BT is turned off,
    let audio manager know about it. This is required for audio
    manager to properly cleanup SCO path.
    
    Change-Id: I111e38cbe578fe3eca66b07da8ad9204d064a02c

--- src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
+++ src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
@@ -1265,6 +1265,10 @@ final class HeadsetClientStateMachine extends StateMachine {
     }
 
     public void doQuit() {
+        Log.d(TAG, "doQuit");
+        if (mAudioManager != null)
+            mAudioManager.setParameters("hfp_enable=false");
+
         quitNow();
     }
 
